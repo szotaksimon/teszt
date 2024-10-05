f5da42c5b4e34b2f8775007dc4a7850dc850a1f2
szotaksimon <szotak.simon16@gmail.com>
2024-10-05 17:07:45 +0200

    submit test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [isSUbmit, setSubmit] = useState(false)
+  const dataContainer = () => {
+    return (
+      <>
+        <div className="input-container">
+          <div className="price-container">
+            <label>1 doboz ára</label>
+            <input type="number" name="price"></input>
+          </div>
+
+          <div className="count-container">
+            <label>Szál / nap</label>
+            <input type="number" name="count"></input>
+          </div>
+        </div>
+
+        <div className="date-container">
+          <label>Lerakás kezdete: </label>
+          <input type="date" name="date"></input>
+        </div>
+      
+      </>
+    )
+  }
+    const congratContainer = () => {
+      return (
+        <div>
+          <p>Congratulations!</p>
+        </div>
+      )
+  } 
+
+  const handleSubmit = () => {
+    setSubmit(!isSUbmit)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      {isSUbmit ? congratContainer() : dataContainer()}
+
+      <div className="submit-container">
+        <button onClick={handleSubmit}>Bevitel</button>
+      </div>
+
+
     </div>
   );
 }
-
 export default App;
